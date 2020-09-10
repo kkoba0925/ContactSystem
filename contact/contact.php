91d7c72bb679d380ef1348149ed32d3833f9c279
EC2 Default User <ec2-user@ip-172-31-14-255.ap-northeast-1.compute.internal>
2020-09-10 08:00:38 +0000

    contact table_tag

---
+++ contact/contact.php
@@ -0,0 +1,56 @@
+<table>
+	<tr>
+		<td colspan="2">会社名</td>
+		<td></td>
+	</tr>
+	<tr>
+		<td colspan="2">氏名</td>
+		<td></td>
+	</tr>
+	<tr>
+		<td colspan="2">氏名(かな)</td>
+		<td></td>
+	</tr>
+	<tr>
+		<td colspan="2">郵便番号</td>
+		<td></td>
+	</tr>
+	<tr>
+		<td rowspan="3">住所</td>
+		<td>都道府県</td>
+		<td></td>
+	</tr>
+	<tr>
+		<td>市区町村</td>
+		<td></td>
+	</tr>
+	<tr>
+		<td>番地･建物名など</td>
+		<td></td>
+	</tr>
+	<tr>
+		<td colspan="2">電話番号</td>
+		<td></td>
+	</tr>
+	<tr>
+		<td colspan="2">生年月日</td>
+		<td></td>
+	</tr>
+	<tr>
+		<td colspan="2">メールアドレス</td>
+		<td></td>
+	</tr>
+	<tr>
+		<td colspan="2">ご連絡方法</td>
+		<td></td>
+	</tr>
+	<tr>
+		<td colspan="2">このページをどうやって知りましたか?</td>
+		<td></td>
+	</tr>
+	<tr>
+		<td colspan="2">備考</td>
+		<td></td>
+	</tr>
+</table>
+
